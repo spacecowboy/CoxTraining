0f35a1f5e6a16ab0491ee15f08e8c8db6e31bff6
Jonas Kalderstam <jonask@thep.lu.se>
2011-08-11 14:20:23 +0200

    Deleted old shit
    Signed-off-by: Jonas Kalderstam <jonask@thep.lu.se>

--- src/survival/plotting.py
+++ src/survival/plotting.py
@@ -71,53 +71,6 @@ def kaplanmeier(data, time_column, event_column, output_column, threshold = None
         ax.set_title("Kaplan-Meier survival curve, threshold: " + str(threshold))
         plt.show()
 
-def divide_and_plot(data, target_column, num = 2):
-    ''' Idea is to plot number of patients still alive on the y-axis,
-    against the time on the x-axis. The target column specifies which
-    axis is the time. The data set is divided into num- parts and
-    plotted against each other.
-    '''
-
-    if plt:
-        #Divide set
-        times = [[] for _ in xrange(num)]
-        alive = [[] for _ in xrange(num)]
-
-        avg_time = np.average(data[:, target_column]) - 2
-
-        for time in data[:, target_column]:
-            #if time < avg_time:
-            if time < 5.0 * random():
-                times[0].append(time)
-            else:
-                times[1].append(time)
-
-        times[0] = sorted(times[0])
-        times[1] = sorted(times[1])
-        #Now make list of all time indices
-        all_times = sorted(times[0] + times[1])
-
-        #Count how many are alive at each time
-        for i in xrange(num):
-            for time in all_times:
-                count = 0.0
-                for pattime in times[i]:
-                    if pattime >= time:
-                        count += 1
-                alive[i].append(count / len(times[i]))
-
-        #Now plot times vs alive
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        for i in xrange(num):
-            ax.plot(all_times, alive[i], 'b-')
-        #plt.legend([p], [str(alive[1, -1])], loc = "upper right")
-        leg = ax.legend((str(alive[0, -1]), str(alive[1, -1])), 'upper right')
-        ax.set_xlabel("Time")
-        ax.set_ylabel("Survival ratio")
-        ax.set_title("Survival plot")
-        plt.show()
-
 if __name__ == '__main__':
     filename = "/home/gibson/jonask/Projects/Kaplan-Meier/genetic.csv"
     #P, T = parse_file(filename, targetcols = [4], inputcols = [-1, -2, -3, -4], ignorerows = [0], normalize = True)
